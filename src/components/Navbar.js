8f9c2fe6dfb9e9c15f043eb6f59d6615d8ebdac0
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-08-19 04:01:59 +0530

    Done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import Logo1 from '../assets/images/logo1.png';
 const Navbar = () => (
   <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
     <Link to="/">
-      <img src={Logo1} alt="logo" style={{ width: '70px', height: '70px', margin: '0px 20px' }} />
+      <img src={Logo1} alt="logo" style={{ width: '60px', height: '60px', margin: '0px 20px' }} />
     </Link>
     <Stack
       direction="row"
